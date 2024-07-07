0ca221b09c1a7ce2550413cb6ab0ff4b8d93acce
Quaradam <65819389+Quaradam@users.noreply.github.com>
2024-07-08 02:08:16 +0300

    A

--- commandshit.bat
+++ commandshit.bat
@@ -1,22 +1,21 @@
 @echo off
-setlocal
-
-haxelib set flixel-addons 2.10.0
-haxelib set flixel-tools 1.5.1
-haxelib set flixel-ui 2.5.0
-haxelib set flixel 5.2.2
-haxelib set lime 8.1.1
-haxelib set openfl 9.2.2
-haxelib install hscript
-haxelib set hxvlc
-haxelib set hxcpp 4.3.2
-haxelib remove linc_luajit
-haxelib remove discord_rpc
-haxelib git linc_luajit https://github.com/superpowers04/linc_luajit
+color 0a
+echo INSTALLING/SETTING LIBRARIES
+@echo on
 haxelib git discord_rpc https://github.com/Aidan63/linc_discord-rpc.git
-haxelib install hxvlc 1.2.0 
-haxelib git extension-webm https://github.com/KadeDev/extension-webm
-haxelib install hxcodec 2.5.1
-haxelib git faxe https://github.com/uhrobots/faxe
-
-echo
+haxelib set flixel 5.2.2
+haxelib set flixel-tools 1.5.1
+haxelib set flixel-addons 3.0.2
+haxelib set flixel-ui 2.5.0
+haxelib set sscript 5.2.0
+haxelib set hxcodec 2.6.1
+haxelib set lime 8.0.1
+haxelib git linc_luajit https://github.com/superpowers04/linc_luajit.git
+haxelib set openfl 9.2.1
+haxelib install haxeui-core
+haxelib install haxeui-flixel
+haxelib install tjson
+haxelib install hxcpp-debug-server
+@echo off
+echo DONE
+pause
